3dd8f7b149e2011d8a93346c82ef83845855e4e2
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-06 12:43:02 -0600

    commit tree a8868c320fbc54e8ee602a415f1b93b1a02e47e0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(arr, event){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        let card = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+        newArr.push(card)
+        debugger;
+    }
+    return newArr
+}
+
+
+function countDown(num){
+    let i = 0
+    while (i <= num){
+        console.log(num--);
+    }
+}
+
